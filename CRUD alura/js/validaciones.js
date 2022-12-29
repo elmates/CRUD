61557767bd47f477ad9156b11418fad6a831111e
Pablo Batres <108700909+elmates@users.noreply.github.com>
2022-12-29 17:48:54 -0600

    Add files via upload

---
+++ CRUD alura/js/validaciones.js
@@ -0,0 +1,93 @@
+export function valida(input) {
+  const tipoDeInput = input.dataset.tipo;
+  if (validadores[tipoDeInput]) {
+    validadores[tipoDeInput](input);
+  }
+
+  if(input.validity.valid){
+    input.parentElement.classList.remove('input-container--invalid');
+    input.parentElement.querySelector('.input-message-error').innerHTML = '';
+  }else{
+    input.parentElement.classList.add('input-container--invalid');
+    input.parentElement.querySelector('.input-message-error').innerHTML = monstrarMensajeDeError(tipoDeInput, input)
+  }
+    
+}
+
+const tipoDeErrores = [
+  'valueMissing',
+  'typeMismatch',
+  'patternMismatch',
+  'customError',
+]
+
+const mensajesDeError = {
+  nombre: {
+    valueMissing: 'Debes colocar al menos un nombre'
+  },
+  email: {
+    valueMissing: 'Debes colocar al menos un correo',
+    typeMismatch: 'El correo no es valido'
+  },
+  password: {
+    valueMissing: 'El campo password no puede estar vacio.',
+    patternMismatch: 'Al menos 6 caracteres, máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales.'
+  },
+  nacimiento: {
+    valueMissing: 'Este campo no puede estar vacio.',
+    customError: 'Debes tener al menos 18 anhos de edad'
+  },
+  numero: {
+    valueMissing: 'Este campo no puede estar vacio.',
+    patternMismatch: 'El formato requerido es XXXXXXXXXX 10 numeros'
+  },
+  direccion: {
+    valueMissing: 'Este campo no puede estar vacio.',
+    patternMismatch: 'Este campo debe contener un rango de 10 a 40 caracteres'
+  },
+  ciudad: {
+    valueMissing: 'Este campo no puede estar vacio.',
+    patternMismatch: 'Este campo debe contener un rango de 10 a 40 caracteres'
+  },
+  estado: {
+    valueMissing: 'Este campo no puede estar vacio, ingrese el estado donde vive actualmente',
+    patternMismatch: 'Este campo debe contener un rango de 10 a 40 caracteres'
+  }
+}
+
+
+
+const validadores = {
+  nacimiento: (input) => validarNacimiento(input),
+};
+
+function monstrarMensajeDeError(tipoDeInput, input){
+  let mensaje = ''
+  tipoDeErrores.forEach(error =>{
+    if(input.validity[error]){
+      
+      mensaje = mensajesDeError[tipoDeInput][error];
+    }
+  })
+  return mensaje
+}
+
+function validarNacimiento(input) {
+  const fechaCliente = new Date(input.value);
+  let mensaje = "";
+  if (!mayorDeEdad(fechaCliente)) {
+    mensaje = "Debes tener al menos 18 años de edad";
+  }
+
+  input.setCustomValidity(mensaje);
+}
+
+function mayorDeEdad(fecha) {
+  const fechaActual = new Date();
+  const diferenciaFechas = new Date(
+    fecha.getUTCFullYear() + 18,
+    fecha.getUTCMonth(),
+    fecha.getUTCDate()
+  );
+  return diferenciaFechas <= fechaActual;
+}
